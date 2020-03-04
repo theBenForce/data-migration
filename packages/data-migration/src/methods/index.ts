bdaac1a6b10d4a59ade6a2c5519c2bcee4afac0a
Ben Force <benforce@gmail.com>
2020-03-04 11:47:56 -0600

    fix: remove GetDownScripts

--- packages/data-migration/src/methods/index.ts
+++ packages/data-migration/src/methods/index.ts
@@ -1,6 +1,5 @@
 export { default as loadDrivers } from "./LoadDrivers";
-export { default as getUpScripts } from "./GetUpScripts";
+export { default as getUpScripts } from "./GetScripts";
 export { default as getMigrationsPath } from "./GetMigrationsPath";
 export { default as createScriptContext } from "./CreateScriptContext";
-export { default as getDownScripts } from "./GetDownScripts";
 export { default as processParams } from "./ProcessParams";
