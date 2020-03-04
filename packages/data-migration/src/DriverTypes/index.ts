76e9c507a5279854d9e94f60267662d97978d685
Ben Force <benforce@gmail.com>
2020-03-04 11:15:42 -0600

    fix: update driver type names

--- packages/data-migration/src/DriverTypes/index.ts
+++ packages/data-migration/src/DriverTypes/index.ts
@@ -1,4 +1,4 @@
 export { default as Driver } from "./Driver";
 export { default as UserPoolDriver } from "./UserPool";
-export { default as RDS } from "./RDS";
-export { default as NoSQL } from "./NoSQL";
+export { default as RdsDriver } from "./RDS";
+export { default as NoSqlDriver } from "./NoSQL";
