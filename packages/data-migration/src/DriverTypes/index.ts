2cb6f669ca9d6f08904f3a38562fc485da3f35c7
Ben Force <benforce@gmail.com>
2020-03-04 08:48:13 -0600

    fix: export driver types

--- packages/data-migration/src/DriverTypes/index.ts
+++ packages/data-migration/src/DriverTypes/index.ts
@@ -1,2 +1,4 @@
 export { default as Driver } from "./Driver";
 export { default as UserPoolDriver } from "./UserPool";
+export { default as RDS } from "./RDS";
+export { default as NoSQL } from "./NoSQL";
