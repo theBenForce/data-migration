74da3b14a40b3770c5c6f0bd6107b9116a3ad0d5
Ben Force <benforce@gmail.com>
2020-03-04 08:03:43 -0600

    chore: update default config

--- packages/data-migration-cli/src/default-config.ts
+++ packages/data-migration-cli/src/default-config.ts
@@ -3,16 +3,17 @@ export default `module.exports = {
   migrationDirectory: "migrations",
   stages: {
     prod: {
+      defaultParams: {
+        region: "us-east-1",
+      },
       users: {
         driver: require("dm-driver-dynamodb"),
         params: {
-          region: "us-east-1",
           TableName: {
             processor: require("dm-processor-cf"),
             params: {
               stack: "some-stack-name",
               output: "SomeOutputName",
-              region: "us-east-1"
             }
           }
         }
