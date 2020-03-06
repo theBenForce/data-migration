effe2e4ac309625df845a7828076db27fb369052
Ben Force <benforce@gmail.com>
2020-03-06 09:31:24 -0600

    fix: add header to default config

--- packages/data-migration-cli/src/default-config.ts
+++ packages/data-migration-cli/src/default-config.ts
@@ -1,4 +1,11 @@
+import { format } from "date-fns";
+
 export default (defaultStage = "prod", migrationDirectory = "migrations") => `
+/**
+ * Data Migration script created on ${format(new Date(), "MMMM do, yyyy")} ${
+  process.env.USER ? `by ${process.env.USER}` : ""
+}
+ */
 import { Configuration } from "data-migration";
 
 export default {
