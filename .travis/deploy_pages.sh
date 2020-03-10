0352ad9001a926046cbe5abb830a52e15414f376
Ben <benforce@gmail.com>
2020-03-10 06:14:04 -0500

    fix: copy verification

--- .travis/deploy_pages.sh
+++ .travis/deploy_pages.sh
@@ -1,5 +1,6 @@
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   ./node_modules/.bin/docpress build
   echo "data-migration.js.org" > _docpress/CNAME
+  cp ./docs/.well-known/brave-rewards-verification.txt _docpress/.well-known/brave-rewards-verification.txt
   ./node_modules/.bin/git-update-ghpages -e
 fi
