b2aaa99593016b7f385f7d5ffd2ca803cdbee4ac
Ben Force <benforce@gmail.com>
2020-05-15 09:08:02 -0500

    ci: get semantic-release to run in all directories

---
+++ .releaserc.js
@@ -0,0 +1,33 @@
+const hooks = require("semantic-release-monorepo-hooks");
+const output = hooks();
+
+module.exports = {
+  extends: "semantic-release-monorepo",
+  tagFormat: output.package + "@${version}",
+  branches: ["master", "switch-to-github-workflows"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "**/dist/**",
+            label: "Distribution",
+          },
+          {
+            path: "**/CHANGELOG.md",
+            label: "Changelog",
+          },
+        ],
+      },
+    ],
+  ],
+  monorepo: {
+    analyzeCommits: ["@semantic-release/commit-analyzer"],
+    generateNotes: ["@semantic-release/release-notes-generator"],
+  },
+};
