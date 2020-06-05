39ac650d304791587fd360faeb2ad40167d04f45
Ben Force <benforce@gmail.com>
2020-06-05 14:30:16 -0500

    chore(release): update page building

--- .build/deploy_pages.sh
+++ .build/deploy_pages.sh
@@ -1,6 +1,8 @@
 
+yarn install
+yarn bootstrap
+
 yarn docs:build
 echo "data-migration.js.org" > _docpress/CNAME
 mkdir _docpress/.well-known
 cp ./docs/.well-known/brave-rewards-verification.txt _docpress/.well-known/brave-rewards-verification.txt
-yarn git-update-ghpages -e
