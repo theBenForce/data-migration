9eb300589aaa085d481b05050911b1946005d9fc
Ben Force <benforce@gmail.com>
2020-06-25 07:58:21 -0500

    fix: set __dirname and __filename to proper values

--- packages/data-migration/src/MigrationScript.ts
+++ packages/data-migration/src/MigrationScript.ts
@@ -6,11 +6,14 @@ export type ScriptContext = {
   getDriver: <T extends Driver<any, any>>(name: string) => Promise<T>;
   getDriversUsed: () => Array<string>;
   getConfigValue: (key: string) => string;
+  scriptPath: string;
+  directory: string;
 };
 
 export type MigrationExecutor<T> = (context: ScriptContext, log: Logger) => Promise<T>;
 
 export default interface MigrationScript {
+  filename: string;
   description?: string;
   up: MigrationExecutor<void>;
   down?: MigrationExecutor<void>;
