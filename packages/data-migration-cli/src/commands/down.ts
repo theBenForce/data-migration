f334b1922898fc8baac50f386435ae47003eb6e8
Ben Force <benforce@gmail.com>
2020-06-25 10:36:33 -0500

    feat: add reset command

--- packages/data-migration-cli/src/commands/down.ts
+++ packages/data-migration-cli/src/commands/down.ts
@@ -8,66 +8,30 @@ import createLogger, { logFile } from "../utils/createLogger";
 import { InitializedMigrationScript } from "data-migration/lib/MigrationScript";
 import loadScripts from "../utils/loadScripts";
 import { DefaultFlags } from "../default-flags";
+import createDownMigrationTasks from "../utils/createDownMigrationTasks";
+
 export default class Down extends Command {
-  static description = "run all down migration scripts";
+  static description = "run down migration scripts";
 
   static flags = {
     help: flags.help({ char: "h" }),
     ...DefaultFlags,
   };
 
-  static args = [];
+  static args = [
+    {
+      name: "numberToRun",
+      default: "1",
+      description: "number of down scripts to execute",
+    },
+  ];
 
   async run() {
     const { args, flags } = this.parse(Down);
-    let stage: string = "";
-    let scripts: Array<InitializedMigrationScript>;
-    let context: ScriptContext;
 
-    const tasks = new Listr([
-      {
-        title: `Load configuration`,
-        async task() {
-          const scriptDetails = await loadScripts(flags);
-          scripts = scriptDetails.scripts;
-          context = scriptDetails.context;
-          stage = scriptDetails.stage;
-        },
-      },
-      {
-        title: `Running Down Migrations`,
-        task(ctx, task) {
-          const filteredScripts = scripts
-            .filter((script) => script.hasRun)
-            .sort((a, b) => (a.name > b.name ? -1 : 1));
+    const numberToRun = parseInt(args.numberToRun);
 
-          task.title = `Running ${filteredScripts.length} Down Migrations on ${stage}`;
-
-          return new Listr(
-            filteredScripts.map((script) => ({
-              title: script.description ?? script.name,
-              task: script.down,
-            }))
-          );
-        },
-      },
-      {
-        title: `Cleanup`,
-        task: () => {
-          const logger = createLogger(["Cleanup"]);
-          return new Listr(
-            context.getDriversUsed().map((driverName: string) => ({
-              title: driverName,
-              async task() {
-                logger(`Cleaning up driver ${driverName}`);
-                const driver = await context.getDriver(driverName);
-                if (driver.cleanup) await driver.cleanup();
-              },
-            }))
-          );
-        },
-      },
-    ]);
+    const tasks = createDownMigrationTasks(flags, numberToRun);
 
     await tasks.run().catch((ex) => {
       createLogger(["ERROR", "CATCH"])(ex.message);
